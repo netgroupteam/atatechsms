93633bb576a176b50bfc1cde335ec2974ae96f80
anarzone <haqverdiyevanar@gmail.com>
2020-07-21 18:46:32 +0400

    added default values

--- config/atatechsms.php
+++ config/atatechsms.php
@@ -2,8 +2,8 @@
 
 return [
     'credentials' => [
-        'login' => env('ATA_TECH_SMS_LOGIN'),
-        'password' => env('ATA_TECH_SMS_PASSWORD'),
-        'host_api' => env('ATA_TECH_SMS_HOST'),
+        'login' => env('ATA_TECH_SMS_LOGIN', true),
+        'password' => env('ATA_TECH_SMS_PASSWORD', true),
+        'host_api' => env('ATA_TECH_SMS_HOST', true),
     ]
 ];
